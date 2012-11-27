de956a213b23ed341d5037b13c3b4c20df20f888
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-11-27 14:53:30 +0100

    Append a simple dom element

--- org.eclipse.rap.customWidget/src/org/eclipse/rap/customWidget/CustomWidget.java
+++ org.eclipse.rap.customWidget/src/org/eclipse/rap/customWidget/CustomWidget.java
@@ -2,6 +2,7 @@ package org.eclipse.rap.customWidget;
 
 import org.eclipse.rap.rwt.internal.remote.RemoteObject;
 import org.eclipse.rap.rwt.internal.remote.RemoteObjectFactory;
+import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
 import org.eclipse.swt.widgets.Composite;
 
 @SuppressWarnings("restriction")
@@ -12,6 +13,7 @@ public class CustomWidget extends Composite {
   public CustomWidget( Composite parent, int style ) {
     super( parent, style );
     ro = RemoteObjectFactory.createRemoteObject( "rap.myWidget" );
+    ro.set( "parent", WidgetUtil.getId( this ) );
   }
 
   @Override
--- org.eclipse.rap.customWidget/src/org/eclipse/rap/customWidget/CustomWidget.js
+++ org.eclipse.rap.customWidget/src/org/eclipse/rap/customWidget/CustomWidget.js
@@ -1,12 +1,22 @@
 rap.registerTypeHandler( "rap.myWidget", {
 
   factory : function( properties ) {
-    console.log( "create" );
-    return {  };
+    var el = document.createElement( "div" );
+    el.style.position = "absolute";
+    el.style.left = "0xp";
+    el.style.top = "0xp";
+    el.style.width = "100%";
+    el.style.height = "100%";
+    el.style.backgroundColor = "#FF7766";
+    el.innerText = "Hello World!"
+    rap.getObject( properties.parent ).append( el );
+    return el;
   },
 
-  destructor : function() {
-    console.log( "destroy" );
+  destructor : function( el ) {
+    if( el.parentNode ) {
+      el.parentNode.removeChild( el );
+    }
   }
 
 } );
